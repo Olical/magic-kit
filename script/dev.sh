26ae68756c2c056c076f3699d58db217360a3e89
Oliver Caldwell <ollie@oli.me.uk>
2021-04-18 16:53:36 +0100

    Add docs to script/dev.sh

--- script/dev.sh
+++ script/dev.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# Used for development of magic kit.
+
+# It sets the config and data directories to be local to this directory. This
+# allows me to run a Neovim instance loading this code as it's configuration
+# without replacing my custom user configuration.
+
+# I basically get a plain fresh instance based on this directory while still
+# having access to the one stored in my home directory.
+
 export XDG_CONFIG_HOME=.dev/config
 export XDG_DATA_HOME=.dev/data
+
 nvim $@
